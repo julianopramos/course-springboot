89620f63d53aa3ad3cdfc3c0a53bcef2ba482d86
Juliano Ramos <jpachecoramos@gmail.com>
2022-01-04 17:53:46 -0300

    Implementado Category...

---
+++ course/src/main/java/br/com/fasttrack/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package br.com.fasttrack.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.fasttrack.course.entities.Category;
+import br.com.fasttrack.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository Repository;
+	
+	public List<Category> findAll(){
+		return Repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = Repository.findById(id);
+		return obj.get();
+	}
+
+}
