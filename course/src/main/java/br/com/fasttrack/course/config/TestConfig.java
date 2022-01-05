7a382203603722c1f5af490995aa39388c0ae35a
Juliano Ramos <jpachecoramos@gmail.com>
2022-01-05 15:39:26 -0300

    Implementamos Entity Payment

--- course/src/main/java/br/com/fasttrack/course/config/TestConfig.java
+++ course/src/main/java/br/com/fasttrack/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.fasttrack.course.entities.Category;
 import br.com.fasttrack.course.entities.Order;
 import br.com.fasttrack.course.entities.OrderItem;
+import br.com.fasttrack.course.entities.Payment;
 import br.com.fasttrack.course.entities.Product;
 import br.com.fasttrack.course.entities.User;
 import br.com.fasttrack.course.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 
