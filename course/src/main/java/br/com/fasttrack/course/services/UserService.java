2c68ceda0a4d04d48445f97bd5fe47bd3867c4ad
Juliano Ramos <jpachecoramos@gmail.com>
2022-01-06 09:47:54 -0300

    Tratando exceptions get user

--- course/src/main/java/br/com/fasttrack/course/services/UserService.java
+++ course/src/main/java/br/com/fasttrack/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.fasttrack.course.entities.User;
 import br.com.fasttrack.course.repositories.UserRepository;
+import br.com.fasttrack.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> user = repository.findById(id);
-		return user.get();
+		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
